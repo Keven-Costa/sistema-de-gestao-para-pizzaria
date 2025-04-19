9d7a8f33ecccc54d668363269b5b7be0cf300e1d
Keven Costa <ogkeven278@gmail.com>
2025-04-18 21:23:29 -0300

    html atualizado

--- src/main/java/com/company/pizzaria/controller/AdminController.java
+++ src/main/java/com/company/pizzaria/controller/AdminController.java
@@ -1,23 +1,41 @@
 package com.company.pizzaria.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-
+    
     @GetMapping("/dashboard")
-    public String dashboard(Model model) {
-        // Removido @PreAuthorize
-        model.addAttribute("titulo", "Painel Administrativo");
+    public String dashboard() {
         return "admin/dashboard";
     }
 
-    @GetMapping("/login")
+    @GetMapping({"/login", "/"})
     public String loginPage() {
         return "admin/login";
+    }
+    
+    @GetMapping("/gestao-ingredientes")
+    public String gestaoIngredientesPage() {
+        return "admin/gestao-ingredientes";
+    }
+    
+    @GetMapping("/gestao-pedidos")
+    public String gestaoPedidoPage() {
+        return "admin/gestao-pedidos";
+    }
+    
+    @GetMapping("/painel-promocoes")  // Alterado para coincidir com o HTML
+    public String painelPromocoes() {
+        return "admin/painel-promocoes";
+    }
+    // Adicione também para Configurações e Sair se necessário
+    @GetMapping("/configuracoes")
+    public String configuracoes() {
+        return "admin/test";
     }
 }
--- src/main/java/com/company/pizzaria/controller/ClientController.java
+++ src/main/java/com/company/pizzaria/controller/ClientController.java
@@ -4,10 +4,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class ClientController {
     
     @GetMapping("/")
     public String mostrarHome() {
         return "forward:/index.html"; // Redireciona para o arquivo estático
     }
+    
+    @GetMapping("/checkout")
+    public String mostrarCheckout() {
+        return "forward:/checkout.html"; // Redireciona para o arquivo estático
+    }
+    
+    @GetMapping("/confirmacao-pedido")
+    public String mostrarConfirmacaoPedido() {
+        return "forward:/confirmacao-pedido.html"; // Redireciona para o arquivo estático
+    }
+    
+    @GetMapping("/montagem-pedido")
+    public String mostrarMontagemPedido() {
+        return "forward:/montagem-pedido.html"; // Redireciona para o arquivo estático
+    }
+    
+    @GetMapping("/rastreamento-pedido")
+    public String mostrarRastreamentoPedido() {
+        return "forward:/rastreamento-pedido.html"; // Redireciona para o arquivo estático
+    }
+    
 }
